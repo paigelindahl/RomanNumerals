0409220af8b91ba90d66d185c3bbb9cc5bcc4c37
Paige Lindahl <paige@peakfifteen.co.uk>
2024-07-23 20:23:41 +0100

    adds date picker to tab

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,24 @@
 <template>
-    <h1 class="text-xl">
-        test
-    </h1>
+    <div class="max-w-7xl mx-auto px-4 md:px-6">
+        <TabComponent :tabs="['Date to Roman', 'Roman to Date']">
+            <template #tab-0>
+            <div>
+                <DateConverter></DateConverter>
+            </div>
+            </template>
+            <template #tab-1>
+            <div>
+                <h2>Tab 2 Content</h2>
+                <p>This is the content for Tab 2.</p>
+            </div>
+            </template>
+        </TabComponent>
+    </div>
 </template>
+
+<script setup>
+import TabComponent from "@/Components/tabs/TabComponent.vue";
+import DateConverter from "@/Components/roman-converters/DateConverter.vue";
+
+
+</script>
---
+++ resources/js/Components/roman-converters/DateConverter.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="max-w-96">
+        <VueDatePicker
+            v-model="date" placeholder="Select Day"
+            model-type="dd.mm.yyyy"
+            :enable-time-picker="false"
+            auto-apply
+        ></VueDatePicker>
+    </div>
+</template>
+
+<script setup>
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+import { ref } from 'vue';
+
+const date = ref();
+</script>
