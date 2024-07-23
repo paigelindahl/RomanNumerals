a1381168cb417e01de990ce96f45fd5aee73f12a
Paige Lindahl <paige@peakfifteen.co.uk>
2024-07-23 23:15:08 +0100

    adds frontend date converter

--- resources/js/Components/roman-converters/DateConverter.vue
+++ resources/js/Components/roman-converters/DateConverter.vue
@@ -1,18 +1,57 @@
 <template>
-    <div class="max-w-96">
-        <VueDatePicker
-            v-model="date" placeholder="Select Day"
-            model-type="dd.mm.yyyy"
-            :enable-time-picker="false"
-            auto-apply
-        ></VueDatePicker>
+    <div class="flex justify-center my-20">
+        <TwoColGrid :romanDate="romanDate" :formattedDate="formattedDate">
+            <template v-slot:input-container>
+                <div class="flex flex-col space-y-3">
+                    <div>
+                        <label class="text-lg text-gray-600">Date</label>
+                        <VueDatePicker
+                            v-model="date"
+                            placeholder=""
+                            :enable-time-picker="false"
+                            :format="format"
+                            auto-apply
+                        ></VueDatePicker>
+                    </div>
+                    <div class="flex justify-end">
+                        <button
+                            @click="convertRoman"
+                            :disabled="!formattedDate"
+                            class="w-fit bg-blue-500 hover:bg-blue-600 transition-all duration-500 px-3 py-2 rounded-lg text-white"
+                        >Convert</button>
+                    </div>
+                </div>
+            </template>
+        </TwoColGrid>
     </div>
 </template>
 
 <script setup>
+import TwoColGrid from "@/Components/grid/TwoColGrid.vue";
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
 import { ref } from 'vue';
+import axios from 'axios';
 
-const date = ref();
+const date = ref(null);
+const formattedDate = ref(null);
+const romanDate = ref(null);
+
+const format = (date) => {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  formattedDate.value = `${day}-${month}-${year}`;
+  return formattedDate.value;
+}
+
+const convertRoman = function() {
+    axios.post('convert-date', {date: formattedDate.value})
+    .then((res) => {
+        romanDate.value = res.data.roman_date;
+        console.log('success', res);
+    }).catch((err) => {
+        console.log('err', err);
+    })
+}
 </script>
