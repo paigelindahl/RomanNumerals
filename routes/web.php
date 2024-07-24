e883dd383a9f0ec5b7d2f5d6610f91d64c7a4117
Paige Lindahl <paige@peakfifteen.co.uk>
2024-07-24 15:03:29 +0100

    refactors routes

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,8 @@ use App\Http\Controllers\DateConversionController;
 Route::get('/', function () {
     return view('app');
 })->name('home');
-Route::post('/convert-date', [DateConversionController::class, 'convertDate']);
+
+Route::controller(DateConversionController::class)->group(function () {
+    Route::post('/convert-date', 'convertDateToRoman')->name('convert.toRoman');
+    Route::post('/convert-numerals', 'convertRomanToDate')->name('convert.toDate');
+});
